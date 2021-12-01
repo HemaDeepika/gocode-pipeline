5e35f8f28bf1f240cabbc97638a308cdc6041998
Hema Deepika <hemadeepikagorre@gmail>
2021-12-01 19:45:55 +0530

    fixed branch issue and main-test

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestHelloWorld(t *testing.T) {
-	if helloworld() != "Hello World!!" {
+	if helloworld() != "Hello World!! How are you world??" {
 		t.Fatal("Test fail")
 	}
 }
